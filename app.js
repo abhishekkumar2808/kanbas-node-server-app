68d7518ace619fa9e4b30ddc08860f46c2061e02
abhishekkumar <kumar.abhishe@northeastern.edu>
2023-11-11 00:56:38 -0500

    Done until 4.3.1 modules/client.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from 'express'
 import Hello from "./hello.js"
 import Lab5 from "./lab5.js";
 import CourseRoutes from './courses/routes.js';
+import ModuleRoutes from "./Modules/routes.js";
+
 import cors from "cors";
 
 
@@ -9,6 +11,7 @@ const app = express()
 app.use(cors());
 app.use(express.json())
 CourseRoutes(app);
+ModuleRoutes(app);
 Lab5(app);
 Hello(app);
 app.listen(4000);
