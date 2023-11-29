f03085ad4cf586945a701e4522f22ea257135c1a
abhishekkumar <kumar.abhishe@northeastern.edu>
2023-11-29 08:55:07 -0500

    env

--- app.js
+++ app.js
@@ -22,12 +22,7 @@ app.use(cors(
  }  
 ));
 
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//   next();
-// });
+
 
 const sessionOptions = {
   secret: "any string",
@@ -45,6 +40,8 @@ if (process.env.NODE_ENV !== "development") {
 app.use(session(sessionOptions));
 
 app.use(express.json())
+
+app.get('/env', (req, res) => res.send(process.env));
 
 UserRoutes(app);
 CourseRoutes(app);
